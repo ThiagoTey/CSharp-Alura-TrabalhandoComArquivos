0153afe7d34078fd247f48f731c8f3ad27590b81
Thiago <teyextreme@outlook.com>
2025-02-10 14:23:22 -0300

    Documentando a classe gerar pix

--- bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
+++ bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
@@ -1,12 +1,24 @@
 ﻿namespace bytebank_GeradorChavePix;
 
+/// <summary>
+/// Classe que gera chaves Pix utilizando o formato Guid
+/// </summary>
 public static class GeradorPix
 {
+    /// <summary>
+    /// Metodo que retorna uma chave aleatória de Pix.
+    /// </summary>
+    /// <returns>Retorna uma chave Pix no formato String</returns>
     public static string getChavePix()
     {
         return Guid.NewGuid().ToString();
     }
 
+    /// <summary>
+    /// Metodo que retorna uma lista de aleatória chaves pix.
+    /// </summary>
+    /// <param name="numeroChaves">Quantidade de chaves a serem retornadas.</param>
+    /// <returns>Lista de string</returns>
     public static List<string> GetChavesPix(int numeroChaves)
     {
         if(numeroChaves <= 0)
