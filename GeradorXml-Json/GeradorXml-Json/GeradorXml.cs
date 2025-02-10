2fd224352c00b95d7a01cc1049e75144f34bedc5
Thiago <teyextreme@outlook.com>
2025-02-10 12:34:48 -0300

    Biblioteca de gerador Xml

---
+++ GeradorXml-Json/GeradorXml-Json/GeradorXml.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace GeradorXml_Json;
+
+public class GeradorXml
+{
+    public static void GerarXml<T>(T objeto) where T : class
+    {
+        string caminhoArquivo = "ArquivoXml.xml";
+
+        try
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var sw = new StreamWriter(fs))
+            {
+                xmlSerializer.Serialize(sw, objeto);
+            }
+
+            Console.WriteLine("Arquivo Gerado com sucesso!");
+        } catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao exportar Xml: {ex.Message}");
+        }
+    }
+
+    public static void GerarArquivosXml<T>(List<T> listaObjeto) where T : class
+    {
+        string caminhoArquivo = "ArquivoClasseXml.xml";
+
+        try
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<T>));
+            using (var fs = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var sw = new StreamWriter(fs))
+            {
+                xmlSerializer.Serialize(sw, listaObjeto);
+            }
+
+            Console.WriteLine("Arquivo Gerado com sucesso!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao exportar Xml: {ex.Message}");
+        }
+    }
+
+}
--- curso_bibliotecas-Aula01/bytebank_ATENDIMENTO/Program.cs
+++ curso_bibliotecas-Aula01/bytebank_ATENDIMENTO/Program.cs
@@ -1,13 +1,27 @@
-﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Atendimento;
 using bytebank_GeradorChavePix;
+using GeradorXml_Json;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 //new ByteBankAtendimento().AtendimentoCliente();
 
-Console.WriteLine(GeradorPix.getChavePix());
+//Console.WriteLine(GeradorPix.getChavePix());
 
-var listaChaves = GeradorPix.GetChavesPix(5);
+//var listaChaves = GeradorPix.GetChavesPix(5);
 
-foreach(var chave in listaChaves)
-{
-    Console.WriteLine(chave);
-}
+//foreach(var chave in listaChaves)
+//{
+//    Console.WriteLine(chave);
+//}
+
+ContaCorrente contaTeste = new(1234, "12345-123412");
+ContaCorrente contaTeste1 = new(412, "34534-1asd");
+ContaCorrente contaTeste2 = new(3145, "1245-5436");
+ContaCorrente contaTeste3 = new(4567, "24567-5678");
+ContaCorrente contaTeste4 = new(124123, "2458-2345");
+
+List<ContaCorrente> listaDeContas = new() { contaTeste, contaTeste1, contaTeste2, contaTeste3, contaTeste4 };
+
+//GeradorXml.GerarXml(contaTeste);
+
+GeradorXml.GerarArquivosXml(listaDeContas);
