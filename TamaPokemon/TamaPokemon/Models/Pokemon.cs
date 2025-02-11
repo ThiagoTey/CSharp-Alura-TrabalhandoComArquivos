12dbec5b1315bb047fd068231b41a2000faf28c3
Thiago <teyextreme@outlook.com>
2025-02-11 14:27:08 -0300

    TamaPokemon

---
+++ TamaPokemon/TamaPokemon/Models/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TamaPokemon.Models;
+
+public class Pokemon
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class PokemonResponse
+{
+    [JsonPropertyName("results")]
+    public List<Pokemon> Results { get; set; } = new();
+}
---
+++ TamaPokemon/TamaPokemon/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.Json;
+using TamaPokemon.Models;
+
+async Task ShowPokemonList()
+{
+    using HttpClient client = new HttpClient();
+
+    try
+    {
+        string url = "https://pokeapi.co/api/v2/pokemon";
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+
+        PokemonResponse pokemonData = JsonSerializer.Deserialize<PokemonResponse>(jsonResponse);
+
+        foreach (Pokemon pokemon in pokemonData.Results)
+        {
+            Console.WriteLine(pokemon.Name);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Aconteceu uma exeção : " + ex.Message);
+        throw;
+    }
+}
+
+await ShowPokemonList();
