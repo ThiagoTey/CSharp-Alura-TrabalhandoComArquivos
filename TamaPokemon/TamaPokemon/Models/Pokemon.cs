1f152ba64a8fddbdeb0f52d4ae09543b7ecd8893
Thiago <teyextreme@outlook.com>
2025-02-12 12:10:19 -0300

    Update na classe Pokemon

--- TamaPokemon/TamaPokemon/Models/Pokemon.cs
+++ TamaPokemon/TamaPokemon/Models/Pokemon.cs
@@ -5,6 +5,10 @@ namespace TamaPokemon.Models;
 public class Pokemon
 {
     public string Name { get; set; }
+    public int Id { get; set; }
+    public int Weight { get; set; }
+    public int Height { get; set; }
+    public List<AbilityWrapper> Abilities { get; set; } = new();
 }
 
 public class PokemonResponse
@@ -12,4 +16,18 @@ public class PokemonResponse
     public List<Pokemon> Results { get; set; } = new();
     public string Next { get; set; } = string.Empty;
     public string Previous { get; set; } = string.Empty;
+}
+
+public class AbilityWrapper
+{
+    public Ability Ability { get; set; }
+    [JsonPropertyName("is_hidden")]
+    public Boolean IsHidden { get; set; }
+    public int Slot { get; set; }
+}
+
+public class Ability
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
 }
--- TamaPokemon/TamaPokemon/Program.cs
+++ TamaPokemon/TamaPokemon/Program.cs
@@ -33,4 +33,37 @@ async Task ShowPokemonList()
     }
 }
 
-await ShowPokemonList();
+async Task ShowUniquePokemon(string pokemonName)
+{
+    string url = $"https://pokeapi.co/api/v2/pokemon/{pokemonName}";
+    using HttpClient client = new HttpClient();
+
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true, // Ignora Lestras maiusculas e minusculas
+        };
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+
+        Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(jsonResponse, options);
+
+        Console.WriteLine($"{pokemon.Name} Peso : {pokemon.Weight} - Altura {pokemon.Height} - ID {pokemon.Id}");
+
+        foreach (AbilityWrapper ability in pokemon.Abilities)
+        {
+            Console.WriteLine($"Habilidade : {ability.Ability.Name}");
+        }
+    }
+    catch (Exception)
+    {
+
+        throw;
+    }
+
+}
+
+await ShowUniquePokemon("ditto");
