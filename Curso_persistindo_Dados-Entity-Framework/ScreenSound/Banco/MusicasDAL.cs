ec647f11ff2f4d4d6817289060c0fad56295508c
Thiago <teyextreme@outlook.com>
2025-02-17 11:57:28 -0300

    MusicaDAL

---
+++ Curso_persistindo_Dados-Entity-Framework/ScreenSound/Banco/MusicasDAL.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+
+class MusicasDAL
+{
+    private readonly ScreenSoundContext context;
+    public MusicasDAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+    public IEnumerable<Musica> Listar()
+    {
+        return context.Musicas.ToList();
+    }
+    public void Adicionar(Musica musica)
+    {
+        context.Musicas.Add(musica);
+        context.SaveChanges();
+    }
+    public void Remover(Musica musica)
+    {
+        context.Musicas.Remove(musica);
+        context.SaveChanges();
+    }
+    public void Atualizar(Musica musica)
+    {
+        context.Musicas.Update(musica);
+        context.SaveChanges();
+    }
+    public Musica ProcurarPeloNome(string nome)
+    {
+        return context.Musicas.FirstOrDefault(m => m.Nome == nome);
+    }
+}
