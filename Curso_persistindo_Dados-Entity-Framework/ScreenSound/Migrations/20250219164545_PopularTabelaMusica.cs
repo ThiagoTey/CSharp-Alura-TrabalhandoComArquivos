6eb5037feec62caa371fb4e2555e4450f525ab07
Thiago <teyextreme@outlook.com>
2025-02-19 13:48:53 -0300

    Adicionando mais migrations

---
+++ Curso_persistindo_Dados-Entity-Framework/ScreenSound/Migrations/20250219164545_PopularTabelaMusica.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" } , new object[] { "Duality", 2004 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Wait and Bleed", 1999 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Psychosocial", 2008 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Before I Forget", 2004 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Snuff", 2008 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "The Devil in I", 2014 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Vermilion", 2004 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Dead Memories", 2008 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "All Out Life", 2018 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
