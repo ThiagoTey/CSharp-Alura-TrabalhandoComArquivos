1218d89ce7fd567d41934835b86aa450e8e21cf5
Thiago <teyextreme@outlook.com>
2025-04-23 16:05:11 -0300

    Ädicionando bibliotecas swagger e Utilizando para documentar a API

--- Curso_persistindo_Dados-Entity-Framework/ScreenSound.API/Program.cs
+++ Curso_persistindo_Dados-Entity-Framework/ScreenSound.API/Program.cs
@@ -14,11 +14,19 @@ builder.Services.AddTransient<DAL<Artista>>();
 // Adiciona o serviço o objeto de DAL de Musicas
 builder.Services.AddTransient<DAL<Musica>>();
 
+//Adiciona o serviço do Swagger para documentação da API
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
 app.AddEndPoinsMusicas();
 app.AddEndPointsArtistas();
+
+// https://localhost:7239/Swagger/index.html para ver documentação de todos os endPoints com o Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
